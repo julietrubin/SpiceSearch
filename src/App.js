292f8af4dd82c1308aa3a7a8e923bc27b9a7788c
Juliet Rubin <rubin.juliet@gmail.com>
2022-01-19 00:02:36 -0800

    add spice list added spice list

--- src/App.js
+++ src/App.js
@@ -6,34 +6,69 @@ import "./styles.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    // this.state = { current: "",  spices: [""]};
+    this.state = { current: "", spices: ["chai", "pumpkin"] };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDeleteEntry = this.handleDeleteEntry.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
+  handleKeyPress(event) {
+    if (event.charCode === 8) {
+      // delete
+      let current = event.target.value;
+      this.setState({ current });
+    }
+
+    let current = event.target.value; //this.state.current +
+    this.setState({ current });
+    if (event.charCode === 13) {
+      // enter
+      this.setState({ spices: [...this.state.spices, current] });
+      event.preventDefault();
+    }
+    if (event.charCode === 40) {
+      // down
+    }
+    if (event.charCode === 38) {
+      // up
+    }
   }
 
   handleSubmit(event) {
-    alert("A name was submitted: " + this.state.value);
-    event.preventDefault();
+    // alert("A name was submitted: " + this.state.value);
+    //event.preventDefault();
+  }
+
+  handleDeleteEntry(index) {
+    alert(index);
+    this.setState({ spices: this.state.spices.splice(index, 1) });
   }
 
   render() {
     return (
-      <form className="App" onSubmit={this.handleSubmit}>
-        <label>
-          <input
-            type="text"
-            className="search"
-            value={this.state.value}
-            onChange={this.handleChange}
+      <div className="App">
+        {this.state.spices.map((spice, index) => (
+          <Spice
+            key={index}
+            name={spice}
+            index={index}
+            handleDeleteEntry={this.handleDeleteEntry}
           />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+        ))}
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input
+              type="text"
+              className="search"
+              value={this.state.value}
+              onKeyPress={this.handleKeyPress}
+            ></input>
+          </label>
+          {/* <input type="submit" value="Submit" /> */}
+        </form>
+      </div>
     );
   }
 }
--- src/Spice.js
+++ src/Spice.js
@@ -1,5 +1,17 @@
 import "./styles.css";
 
-export default function Spice() {
-  return <div clasßsName="Spice"></div>;
+export default function Spice(props) {
+  return (
+    <span className="Spice">
+      {props.name}
+      <button
+        className="delete"
+        onClick={() => {
+          props.handleDeleteEntry(props.index);
+        }}
+      >
+        x
+      </button>
+    </span>
+  );
 }
