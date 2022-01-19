5fab4f9aa48dfe14602219c3e743e53a49efd444
Juliet Rubin <rubin.juliet@gmail.com>
2022-01-18 23:06:03 -0800

    Add file input field layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import React from "react";
+
+import Spice from "./Spice.js";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className="App" onSubmit={this.handleSubmit}>
+        <label>
+          <input
+            type="text"
+            className="search"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
+
+export default App;
